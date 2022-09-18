3f0f39605fb6139cea2a86e32cde2d73ac85355e
Angelo Soares <angeelosoares@gmail.com>
2022-09-18 17:37:57 -0300

    chore: category logo

--- src/shared/constants/categories.ts
+++ src/shared/constants/categories.ts
@@ -3,18 +3,21 @@ export const categories = [
     id: 1,
     category: 'Tesla',
     created_at: new Date(),
-    updated_at: new Date()
+    updated_at: new Date(),
+    logo: 'https://cdn.discordapp.com/attachments/788845721797394485/1021146536639483924/tesla.svg'
   },
   {
     id: 2,
     category: 'Audi',
     created_at: new Date(),
-    updated_at: new Date()
+    updated_at: new Date(),
+    logo: 'https://cdn.discordapp.com/attachments/788845721797394485/1021146535339229194/audi.svg'
   },
   {
     id: 3,
     category: 'Volvo',
     created_at: new Date(),
-    updated_at: new Date()
+    updated_at: new Date(),
+    logo: 'https://cdn.discordapp.com/attachments/788845721797394485/1021146536916295781/volvo.svg'
   }
 ]
