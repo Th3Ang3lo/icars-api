a4479367044674d06cf1820aba0b1de381c0d420
Angelo Soares <angeelosoares@gmail.com>
2022-09-18 19:09:16 -0300

    fix: controller adapter headers are down when error

--- src/infra/http/adapters/controller.ts
+++ src/infra/http/adapters/controller.ts
@@ -5,11 +5,22 @@ import { Controller } from '@/domain/contracts/controller'
 export type Adapter = (controller: Controller) => RequestHandler
 
 export const controllerAdapter: Adapter = controller => async (request: Request, response: Response): Promise<Response> => {
-  const dispatchController = await controller.handle(request)
+  try {
+    const dispatchController = await controller.handle(request)
 
-  const status = Object.keys(dispatchController ?? []).length > 0
-    ? 200
-    : 204
+    const status = Object.keys(dispatchController ?? []).length > 0
+      ? 200
+      : 204
 
-  return response.status(status).send(dispatchController)
+    return response.status(status).send(dispatchController)
+  } catch (error) {
+    if (process.env.NODE_ENV === 'dev') {
+      console.log(error)
+    }
+
+    return response.status(error.statusCode || 500).send({
+      message: error.errorMessage || 'Internal Server Error',
+      field: error.param || undefined
+    })
+  }
 }
