819dd20278d152bfbee21617dcef74379647d881
Angelo Soares <angeelosoares@gmail.com>
2022-09-20 10:38:13 -0300

    refactor: new vehicles and categories added to seed

--- src/infra/database/seeds/vehicles.ts
+++ src/infra/database/seeds/vehicles.ts
@@ -79,6 +79,90 @@ void (async () => {
       }
     })
 
+    await prismaClient.vehicles.create({
+      data: {
+        id: 4,
+        name: 'Corvette',
+        categoryId: categories[3].id,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 1deserunt mollit anim id est laborum.',
+        year: '2018',
+        created_at: new Date(),
+        updated_at: new Date(),
+        vehicleImages: {
+          create: [
+            {
+              url: 'https://i.imgur.com/ge65Lyt.png',
+              created_at: new Date()
+            },
+            {
+              url: 'https://i.imgur.com/K92yK9X.png',
+              created_at: new Date()
+            },
+            {
+              url: 'https://i.imgur.com/RqutOc0.png',
+              created_at: new Date()
+            }
+          ]
+        }
+      }
+    })
+
+    await prismaClient.vehicles.create({
+      data: {
+        id: 5,
+        name: 'Lamborghini Huracan',
+        categoryId: categories[4].id,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 1deserunt mollit anim id est laborum.',
+        year: '2022',
+        created_at: new Date(),
+        updated_at: new Date(),
+        vehicleImages: {
+          create: [
+            {
+              url: 'https://i.imgur.com/x8CB1v8.png',
+              created_at: new Date()
+            },
+            {
+              url: 'https://i.imgur.com/noJYM8P.png',
+              created_at: new Date()
+            },
+            {
+              url: 'https://i.imgur.com/9H5ls3C.png',
+              created_at: new Date()
+            }
+          ]
+        }
+      }
+    })
+
+    await prismaClient.vehicles.create({
+      data: {
+        id: 6,
+        name: 'Lancer Evolution GSR',
+        categoryId: categories[5].id,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 1deserunt mollit anim id est laborum.',
+        year: '2015',
+        created_at: new Date(),
+        updated_at: new Date(),
+        vehicleImages: {
+          create: [
+            {
+              url: 'https://i.imgur.com/dyuVAPn.png',
+              created_at: new Date()
+            },
+            {
+              url: 'https://i.imgur.com/cyXPLS0.png',
+              created_at: new Date()
+            },
+            {
+              url: 'https://i.imgur.com/ciIYoU8.png',
+              created_at: new Date()
+            }
+          ]
+        }
+      }
+    })
+
     console.log('Seed de veículos inserida.')
   } catch (error) {
     console.log('Houve um erro ao gerar as seed de categorias. ', error)
--- src/shared/constants/categories.ts
+++ src/shared/constants/categories.ts
@@ -19,5 +19,26 @@ export const categories = [
     created_at: new Date(),
     updated_at: new Date(),
     logo: 'https://cdn.discordapp.com/attachments/788845721797394485/1021146536916295781/volvo.svg'
+  },
+  {
+    id: 4,
+    category: 'Chevrolet',
+    created_at: new Date(),
+    updated_at: new Date(),
+    logo: 'https://cdn.discordapp.com/attachments/788845721797394485/1021146535658012783/chevrolet.svg'
+  },
+  {
+    id: 5,
+    category: 'Lamborghini',
+    created_at: new Date(),
+    updated_at: new Date(),
+    logo: 'https://cdn.discordapp.com/attachments/788845721797394485/1021146535993552977/lamborghini.svg'
+  },
+  {
+    id: 6,
+    category: 'Mitsubishi',
+    created_at: new Date(),
+    updated_at: new Date(),
+    logo: 'https://cdn.discordapp.com/attachments/788845721797394485/1021146536303919195/mitsubishi.svg'
   }
 ]
